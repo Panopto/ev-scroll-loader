fa7bcd45162037626c4617648c5e5b1e937f99d7
Jim Pease <jmpease@codeserv.us>
2013-01-22 07:23:06 -0500

    Removed dangling ',' from package.json.  Updated grunt module versions and rebuilt.

--- dist/ev-scroll-loader.js
+++ dist/ev-scroll-loader.js
@@ -1,4 +1,4 @@
-/*! Ensemble Video jQuery Scroll Loader Plugin - v0.1.0 - 2013-01-18
+/*! Ensemble Video jQuery Scroll Loader Plugin - v0.1.0 - 2013-01-22
 * https://github.com/jmpease/ev-scroll-loader
 * Copyright (c) 2013 Symphony Video, Inc.; Licensed MIT, GPL-2.0 */
 (function($) {
